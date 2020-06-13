1a5abd38c69cfae383a50ce80d5034ecf2e6709b
nakzyu <nakzyu@gmail.com>
2020-06-13 22:14:20 +0900

    Add ProjectInput class

---
+++ src/app.ts
@@ -0,0 +1,57 @@
+// autobind decorator
+
+// ProjectInput Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  // define selection and rough set up in constructor
+  // and  things like insertion  as class method
+  constructor() {
+    // typecasting => <HTMLTemplateElement>document.getElementById('project-input')!;
+    // || document.getElementById('project-input')! as HTMLTemplateElement;
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement; //! => say all is good to ts
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();

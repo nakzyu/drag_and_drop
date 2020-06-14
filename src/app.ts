d8f3fb9810d7868d15e5285e6f7baf7d0536604a
nakzyu <nakzyu@gmail.com>
2020-06-14 16:35:29 +0900

    Define autobind

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 // autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 
 // ProjectInput Class
 class ProjectInput {
